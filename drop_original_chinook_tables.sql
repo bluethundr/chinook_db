1e66190305cf62fe38fa287bc9eb55cf61aa81fa
Timothy Dunphy <Timothy.Dunphy@synchronoss.com>
2020-07-05 17:00:52 -0400

    Added drop table, updated insert statements and added advanced chinook DB.

---
+++ drop_original_chinook_tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE public.user_info CASCADE;
+DROP TABLE public.event_info CASCADE;
+DROP TABLE public.event_log CASCADE;
+DROP TABLE public.test_table CASCADE;
+DROP TYPE day_of_week;
--- insert_statements.sql
+++ insert_statements.sql
@@ -1,1 +1,3 @@
 INSERT INTO public.test_table VALUES (1, 'Monday'), (1, 'Tuesday'), (1, 'Wednesday'), (1, 'Thursday'), (1, 'Friday'), (1, 'Saturday'), (1, 'Sunday');
+
+INSERT INTO public.event_info VALUES (1, 'login'), (2, 'click');
